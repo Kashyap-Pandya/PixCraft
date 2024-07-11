9985de3a87565c3875cc5444b0830bd826b97cc8
Kashyap Pandya <thecodezx@gmail.com>
2024-07-11 11:48:53 +0530

    [IMP] fixed eslint warning

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <div>
       <h2>Not Found</h2>
-      <p>Sorry, the page you're looking for does not exist. </p>
+      <p>Sorry, the page you are looking for does not exist. </p>
       <Link href="/">Return Home</Link>
     </div>
   );
